4098a7b3ca3b3fa53ac9d0d44961fa4826903ec7
Naod <naodk36@gmail.com>
2023-11-28 16:15:43 -0800

    new line

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,8 @@
 #auth - Naod
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:7]
+
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
+
+print()
