a2975e5cd27c0ab35c54c34f32b8c1bd7be15365
Naod <naodk36@gmail.com>
2023-11-30 14:18:36 -0800

    mag

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
     add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
     else:
-        return sub(a, b)
+        return (sub(a, b))
 
 # Test
 result = magic_calculation(3, 7)
