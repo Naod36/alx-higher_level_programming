3926675c12e42288f48ff5c383cdee2c53e80053
Naod <naodk36@gmail.com>
2023-11-30 14:15:17 -0800

    m

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-        
     add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
 
     if a < b:
@@ -11,3 +10,8 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
+
+# Test
+result = magic_calculation(3, 7)
+print(result)
+
